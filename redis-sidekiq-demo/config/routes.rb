6872bbd0e31851e6a0ab41d558ebbf8f6d598fce
Trong Tran <tranduytrong2000@gmail.com>
2024-04-29 16:41:11 +0700

    add sidekiq

--- redis-sidekiq-demo/config/routes.rb
+++ redis-sidekiq-demo/config/routes.rb
@@ -1,3 +1,6 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
+  mount Sidekiq::Web => "/sidekiq" # mount Sidekiq::Web in your Rails app
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
